7364473f9c61dcee1bd137a17e8030337cddf50c
umarfarookh <sdumarfarookh212@gmail.com>
2024-05-04 15:08:28 +0530

    added usestate for items

--- App.jsx
+++ App.jsx
@@ -1,10 +1,43 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      item: "tea",
+      cost: 10,
+      quantity: 0,
+    },
+    {
+      item: "coffee",
+      cost: 20,
+      quantity: 0,
+    },
+    {
+      item: "horlicks",
+      cost: 20,
+      quantity: 0,
+    },
+    {
+      item: "Boost",
+      cost: 25,
+      quantity: 0,
+    },
+    {
+      item: "green tea",
+      cost: 20,
+      quantity: 0,
+    },
+    {
+      item: "lemon tea",
+      cost: 15,
+      quantity: 0,
+    },
+    {
+      item: "black tea",
+      cost: 25,
+      quantity: 0,
+    },
+  ]);
 
   return <></>;
 }
