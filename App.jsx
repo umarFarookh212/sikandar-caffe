acdf3931fa25f9b05aff1daac9202c5fc6ec5cab
umarfarookh <sdumarfarookh212@gmail.com>
2024-05-06 13:06:43 +0530

    redesigned table with shadow and padding and align items

--- App.jsx
+++ App.jsx
@@ -19,12 +19,12 @@ function App() {
     },
     {
       item: "Boost",
-      cost: 25,
+      cost: 15,
       quantity: 0,
     },
     {
       item: "green tea",
-      cost: 20,
+      cost: 10,
       quantity: 0,
     },
     {
@@ -34,7 +34,22 @@ function App() {
     },
     {
       item: "black tea",
-      cost: 25,
+      cost: 15,
+      quantity: 0,
+    },
+    {
+      item: "milk",
+      cost: 12,
+      quantity: 0,
+    },
+    {
+      item: "samosa",
+      cost: 10,
+      quantity: 0,
+    },
+    {
+      item: "plain cake",
+      cost: 10,
       quantity: 0,
     },
   ]);
@@ -49,7 +64,7 @@ function App() {
     const newItems = [...items];
     newItems[idx].quantity >= 1 ? (newItems[idx].quantity -= 1) : " ";
     setItems(newItems);
-    decreseQuantity(items);
+    decreaseQuantity(items);
     decrementTotal(items);
   }
   function increaseQuantity(items) {
@@ -59,7 +74,7 @@ function App() {
     }
     return totalCount;
   }
-  function decreseQuantity(items) {
+  function decreaseQuantity(items) {
     let totalCount = 0;
     for (const item of items) {
       totalCount -= item.quantity;
@@ -83,70 +98,78 @@ function App() {
   }
   return (
     <>
-      <div class="p-5 bg-gray-100 min-h-full">
-        <div class="overflow-auto rounded-lg shadow hidden md:block">
-          <table>
-            <thead class="bg-gray-450 border-2 border-gray-200">
-              <tr>
-                <th class="w-20 p-3 text-sm font-bold tracking-wide text-left">
-                  No
-                </th>
-                <th class="w-32 p-3 text-sm font-bold tracking-wide text-left">
-                  Item
-                </th>
-                <th class="w-20 p-3 text-sm font-bold tracking-wide text-left">
-                  Quantity
-                </th>
-                <th class="w-32 p-3 text-sm font-bold tracking-wide text-left">
-                  Cost
-                </th>
-                <th class="w-32 p-3 text-sm font-bold tracking wide text-left">
-                  Total
-                </th>
-              </tr>
-            </thead>
-            <tbody class="divide-y divide-gray-100">
-              {items.map((item, idx) => (
-                <tr class="bg-gray-300 " key={item.item}>
-                  <td class="p-3 text-sm text-gray-700 whitespace-nowrap ">
-                    {idx + 1}
-                  </td>
-                  <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    {item.item}
-                  </td>
-                  <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                    {item.quantity}
-                  </td>
-                  <td class="p-3 text-sm text-gray-750 whitespace-nowrap">
-                    RS. {item.cost}
-                  </td>
-                  <td class="p-3 text-sm text-gray-750 whitespace-nowrap ">
-                    {item.quantity * item.cost}
-                  </td>
-                  <td>
-                    <button onClick={() => decrement(idx)}>-</button>
-                  </td>
-                  <td>
-                    <button onClick={() => increment(idx)}>+</button>
-                  </td>
+      <div class="p-5 min-h-full mx-2">
+        <div>
+          <div class="overflow-auto hidden md:block shadow-lg shadow-indigo-400/90 max-w-2xl mx-44">
+            <table class="border- border-blue max-w-2xl">
+              <thead class="bg-blue-950 text-white font-mono tracking-wide text-md">
+                <tr>
+                  <th class="w-20 p-3 text-left">No</th>
+                  <th class="w-36 p-3 text-left">Cost</th>
+                  <th class="w-56 p-3 text-left ">Item</th>
+                  <th class="w-36 p-3 text-left">Total</th>
+                  <th></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody class="divide-y divide-gray-300 font-bold bg-white text-blue-950 tracking-wide text-sm">
+                {items.map((item, idx) => (
+                  <tr key={item.item}>
+                    <td class="p-3 oldstyle-nums">{idx + 1}</td>
+                    <td class="p-3 whitespace-nowrap uppercase">{item.item}</td>
+                    <td class="p-3 whitespace-nowrap">₹{item.cost}</td>
+                    <td class="p-3 whitespace-nowrap ">
+                      {item.quantity * item.cost}
+                    </td>
+                    <div class="p-3">
+                      <td>
+                        <button
+                          onClick={() => decrement(idx)}
+                          class="rounded-full bg-blue-950 min-w-7 min-h-7 text-center text-cyan-100 font-bold text-xl"
+                        >
+                          -
+                        </button>
+                      </td>
+                      <td class="p-3 text-sm">{item.quantity}</td>
+                      <td>
+                        <button
+                          onClick={() => increment(idx)}
+                          class="rounded-full bg-blue-950 min-w-7 min-h-7 text-center text-white font-bold text-xl"
+                        >
+                          +
+                        </button>
+                      </td>
+                    </div>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div class="grid grid-cols-1 gap-4 md:hidden min-w-full border border-black p-3">
-          <div class="bg-amber-50 space-y-2 p-2 capitalize rounded-xl shadow-2xl border-2 border-green-300 sticky top-3 text-teal-900 font-bold">
+        <div class="bg-transparent  rounded-3xl shadow-2xl border-2 border-blue-600 sticky bottom-5 font-bold max-w-lg hidden md:block">
+          <div class="flex flex-col content-end space-y-2 p-2">
+            <div class="bg-slate-300 w-44 px-3  rounded-3xl">
+              Total Quantity : {increaseQuantity(items)}
+            </div>
+            <div class="bg-slate-300 w-44 px-3 rounded-3xl">
+              Total Amount : ₹{incrementTotal(items)}
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        {/* mobile app */}
+        {/*  */}
+        <div class="grid grid-cols-1 gap-2 md:hidden min-w-full border-2 border-black rounded-3xl p-2">
+          <div class="bg-amber-50 space-y-2 p-2 capitalize rounded-xl shadow-2xl border-2 border-green-600 sticky top-3 text-teal-900 font-semibold">
             <div>Total Quantity : {increaseQuantity(items)}</div>
-            <div>Total Amount : {incrementTotal(items)}</div>
+            <div>Total Amount : ₹{incrementTotal(items)}</div>
           </div>
           {items.map((item, idx) => (
-            <div class="bg-white space-y-4 p-4 rounded-xl shadow-xl border border-sky-800">
-              console.log(idx)
+            <div class="bg-white space-y-4 p-4 rounded-2xl shadow-xl border-2 border-sky-900">
               <div class="flex justify-between items-center space-x-2">
                 {/* <div class=>
                   {1 + idx}
                 </div> */}
-                <div class="font-medium font-serif text-black uppercase w-36 border-2 hover:italic  ">
+                <div class="font-medium font-serif text-black uppercase w-36  hover:italic  ">
                   {item.item}
                 </div>
                 <div class="flex text-xl w-28 rounded-2xl bg-red-300	">
@@ -160,12 +183,12 @@ function App() {
                 </div>
               </div>
               <div class="flex justify-between items-center">
-                <div class="text-sm font-bold text-black-700 w-12 border ">
-                  {item.cost}
+                <div class="text-sm font-bold text-black-700 w-12 ">
+                  ₹{item.cost}
                 </div>
 
-                <div class="text-bold-gray-700 w-24 border text-right">
-                  {item.quantity * item.cost}
+                <div class="text-bold-gray-700 w-24 text-right">
+                  ₹{item.quantity * item.cost}
                 </div>
               </div>
             </div>
